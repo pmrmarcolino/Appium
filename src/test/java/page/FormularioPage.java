2100dc8550be928c07fe36662c1d2df53fee66f1
patricia <pmrmarcolino@hotmail.com>
2019-01-18 17:23:36 -0200

    Estruturação utilizando herança

--- src/test/java/page/FormularioPage.java
+++ src/test/java/page/FormularioPage.java
@@ -1,38 +1,36 @@
 package page;
 
-import core.DSL;
+import core.BasePage;
 import io.appium.java_client.MobileBy;
 import org.openqa.selenium.By;
 
 import java.net.MalformedURLException;
 
 // não deve ter asserts
-public class FormularioPage {
-
-    private  DSL dsl = new DSL();
+public class FormularioPage extends BasePage {
 
     public void escreverNome(String nome) throws MalformedURLException {
-        dsl.escrever(MobileBy.AccessibilityId("nome"), nome);
+        escrever(MobileBy.AccessibilityId("nome"), nome);
     }
 
     public void selecionarCombo(String combo) throws MalformedURLException {
-        dsl.selecionarCombo(By.className("android.widget.Spinner"), combo );
+        selecionarCombo(By.className("android.widget.Spinner"), combo );
     }
 
     public void selecionarCheck(String opcao) throws MalformedURLException {
-        dsl.selecionarCheckSwitch(opcao);
+        selecionarCheckSwitch(opcao);
     }
 
     public void selecionarSalvar(String botao) throws MalformedURLException {
-        dsl.clickBotao(By.xpath("//*[@text='"+botao+"']"));
+        clickBotao(By.xpath("//*[@text='"+botao+"']"));
     }
 
 
     public String confirmDadosGridIndex(String index) throws MalformedURLException {
-        return dsl.buscaPorAtributo(By.xpath("//android.widget.TextView[@index='" + index + "']"), "text");
+        return buscaPorAtributo(By.xpath("//android.widget.TextView[@index='" + index + "']"), "text");
     }
     public boolean confirmaDadosGridFimString(String end,String tipo) throws MalformedURLException {
-        return dsl.buscaPorFimString(By.xpath("//android.widget.TextView[starts-with(@text, '"+tipo+": ')]"), end);
+        return buscaPorFimString(By.xpath("//android.widget.TextView[starts-with(@text, '"+tipo+": ')]"), end);
     }
 
 }
