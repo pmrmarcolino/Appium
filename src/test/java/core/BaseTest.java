5b10b5087f1a1843c960ce3bdab24f34248ba5d9
patricia <pmrmarcolino@hotmail.com>
2019-01-18 18:06:20 -0200

    Screenshot

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -1,13 +1,23 @@
 package core;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 
 import static core.DriverFactory.*;
 
 public class BaseTest {
+
+    @Rule
+    public TestName testName = new TestName();
 
     @AfterClass // é executado ao final da da execução da classe
     public static void finalizaClasse(){
@@ -15,7 +25,13 @@ public class BaseTest {
     }
 
     @After // é executado sempre ao final de cada método
-    public void tearDown() throws MalformedURLException {
+    public void tearDown() throws IOException {
+        gerarScreenshot();
         getDriver().resetApp(); // para não matar a sessão do app a cada execução de métodos de teste
+    }
+
+    public void gerarScreenshot() throws IOException {
+        File image = ((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(image, new File("target/screenshots/"+testName.getMethodName()+".png"));
     }
 }
