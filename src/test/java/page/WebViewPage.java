8fe699c089e6ccf04fdc00386159b877377cb583
Patricia <tqi_pramos@uolinc.com>
2019-09-25 15:06:30 -0300

    webView

--- src/test/java/page/WebViewPage.java
+++ src/test/java/page/WebViewPage.java
@@ -1,24 +1,31 @@
 package page;
 
-import com.oracle.tools.packager.Log;
 import core.BasePage;
 import org.openqa.selenium.By;
-
 import java.net.MalformedURLException;
 import java.util.Set;
+
+import java.util.logging.Logger;
 
 import static core.DriverFactory.getDriver;
 
 public class WebViewPage extends BasePage {
 
     public void entrarContextoWeb() throws MalformedURLException {
-        String context = getDriver().getContext();
-            Log.info(context);
-
-        //getDriver().context((String) contexHandlres.toArray()[1]);
+        Logger Logger = java.util.logging.Logger.getLogger(WebViewPage.class.getName());
+        Set<String> context =  getDriver().getContextHandles();
+        Logger.info((String) context.toArray()[1]);
+        getDriver().context((String) context.toArray()[1]);
     }
 
-    public void setEmail() throws MalformedURLException {
-        getDriver().findElement(By.id("email"));
+    public void setEmail(String valor) throws MalformedURLException {
+        getDriver().findElement(By.id("email")).sendKeys(valor);
     }
+
+    public void sairContextoWeb() throws MalformedURLException {
+        Set<String> context =  getDriver().getContextHandles();
+        getDriver().context((String) context.toArray()[0]);
+    }
+
+
 }
